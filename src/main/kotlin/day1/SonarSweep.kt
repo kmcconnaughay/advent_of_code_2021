239982a03707b77dd2696db145bf77249a96b907
kmcconnaughay <kyle.mcconnaughay@gmail.com>
2021-12-01 12:52:57 +0100

    Add documentation to Day 1 functions

--- src/main/kotlin/day1/SonarSweep.kt
+++ src/main/kotlin/day1/SonarSweep.kt
@@ -13,12 +13,22 @@ fun numDepthMeasurementIncreases(depthMeasurements: List<Int>): Int {
         .count { (firstDepthMeasurement, secondDepthMeasurement) -> firstDepthMeasurement < secondDepthMeasurement }
 }
 
+/**
+ * Returns the number of strict increases in depth from three-width rolling sums of the given measurements.
+ *
+ * This is Part 2 of the challenge. See the [challenge description](https://adventofcode.com/2021/day/1) for more
+ * information.
+ */
 fun numAveragedDepthMeasurementIncreases(depthMeasurements: List<Int>): Int {
     return depthMeasurements.windowed(size = 3, step = 1, partialWindows = false).map(List<Int>::sum)
         .windowed(size = 2, step = 1, partialWindows = false)
         .count { (firstAverage, secondAverage) -> firstAverage < secondAverage }
 }
 
+/**
+ * Marshals depth measurements from disk. This procedure assumes that the file contains one integer per line with no
+ * other text.
+ */
 fun readDepthMeasurements(): List<Int> {
     val fileName =
         "C:\\Users\\Allie\\IdeaProjects\\advent_of_code_2021\\src\\main\\kotlin\\day1\\depth_measurements.txt"
